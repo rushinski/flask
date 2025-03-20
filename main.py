9bad7128bbcb35684534f64e8c6ea2f25269547e
Jacob Rushinski <dsrush2@verizon.net>
2025-03-20 08:44:29 -0400

    Search function added

--- main.py
+++ main.py
@@ -7,30 +7,36 @@ conn_str = 'mysql://root:cset155@localhost/boatdb'
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
-@app.route('/') # Creates a home route to get the code into the server
-def hello():
-    return render_template('index.html')
-
-@app.route('/<name>') 
-def greeting(name):
-    return render_template('user.html', name = name)
-
 @app.route('/boats')
 def boats():
     boats = conn.execute(text('SELECT * FROM boats')).all()
     return render_template('boats.html', boats=boats)
 
-@app.route('/boatCreate', methods=['GET'])
+@app.route('/boat-create', methods=['GET'])
 def get_boat():
     return render_template('boat_create.html')
 
-@app.route('/boatCreate', methods=['POST'])
+@app.route('/boat-create', methods=['POST'])
 def create_boat():
     try:
         conn.execute(text('INSERT INTO boats VALUES(:id, :name, :type, :owner_id, :rental_price)'), request.form)
         return render_template('boat_create.html', error=None, sucess='Successful')
     except:
         return render_template('boat_create.html', error='Failed', sucess=None)
+    
+@app.route('/boat-search', methods=['GET'])
+def get_boat_search():
+    boats = []
+    return render_template('boat_search.html', boats = boats)
+
+@app.route('/boat-search', methods=['POST'])
+def find_boat_search():
+    try:
+        boats = conn.execute(text('SELECT * FROM boats WHERE id = :id'), request.form).all()
+        return render_template('boat_search.html', boats=boats, error=None, sucess='Sucess')
+    except:
+        return render_template('boat_search.html', boats=boats, error='Falied', sucess=None)
+
 
 # Should always be last 2 lines of code
 if __name__ == '__main__': # Checks to make sure the file that the code is ran on is the Flask main
